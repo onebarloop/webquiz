45b1d7ad49ac738d79bd9489970755431d8e794c
Alexander Bayer <alexanderbayer@posteo.de>
2022-10-25 12:57:45 +0200

    Implement functionality answer btn

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const answerBtn = document.querySelector('[data-js="answerBtn"]');
+
+const answer = document.querySelector('[data-js="answer"]');
+
+answerBtn.addEventListener("click", () => {
+  console.log("Hello World!");
+  answer.classList.remove("answer");
+});
