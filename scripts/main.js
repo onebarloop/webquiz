d63dc5dfb23105aaa47509a141c8fbf7bd52d022
Alexander Bayer <alexanderbayer@posteo.de>
2022-10-25 14:06:21 +0200

    First JS for Darkmode

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//answer button
+
 const answerBtn = document.querySelector('[data-js="answerBtn"]');
 
 const answer = document.querySelector('[data-js="answer"]');
@@ -6,3 +8,13 @@ answerBtn.addEventListener("click", () => {
   console.log("Hello World!");
   answer.classList.toggle("answer");
 });
+
+//darkmode button
+
+// const darkSwitch = document.querySelector('[data-js="darkBtn"]');
+
+// const darkStyle = document.querySelector('[data-js="darkstyle"]');
+
+//darkSwitch.addEventListener("click", () => {
+//  console.log("Darkmode enable");
+// });
