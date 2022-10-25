1038ad191423201f58b3aa9d5c51e984cd98dfac
Alexander Bayer <alexanderbayer@posteo.de>
2022-10-25 15:21:17 +0200

    Add console.log

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,6 @@ const answerBtn = document.querySelector('[data-js="answerBtn"]');
 const answer = document.querySelector('[data-js="answer"]');
 
 answerBtn.addEventListener("click", () => {
-  console.log("Hello World!");
+  console.log("Show Answer");
   answer.classList.toggle("answer");
 });
