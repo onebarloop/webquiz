637bb4c35c61dbf5f27e7a49b5c75b5fe21696bc
Alexander Bayer <115539625+onebarloop@users.noreply.github.com>
2022-10-25 18:31:50 +0200

    Start working on JS (#5)
    * Add JS file
    
    * Implement functionality answer btn
    
    * add toggle method
    
    * First JS for Darkmode
    
    * Implement (bad) darkmode
    
    * Add console.log
    
    * Fix answer button2
    
    * fix answer button 2

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+//answer button
+
+const answerBtn = document.querySelector('[data-js="answerBtn"]');
+
+const answer = document.querySelector('[data-js="answer"]');
+
+answerBtn.addEventListener("click", () => {
+  console.log("Show Answer");
+  answer.classList.toggle("hidden");
+});
+
+const answerBtn2 = document.querySelector('[data-js="answerBtn2"]');
+
+const answer2 = document.querySelector('[data-js="answer2"]');
+
+answerBtn2.addEventListener("click", () => {
+  console.log("Show Answer");
+  answer2.classList.toggle("hidden");
+});
