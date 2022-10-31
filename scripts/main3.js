e49f01045bfcc2b9b71f52c56d6dcd7b7aaab052
Alexander Bayer <115539625+onebarloop@users.noreply.github.com>
2022-10-31 10:58:48 +0100

    Feature/bookmark func (#9)
    * Not working...
    
    * Fixed darkmode

---
+++ scripts/main3.js
@@ -0,0 +1,34 @@
+// add bookmarked questions
+
+//const question = document.querySelector('[data-js="test"]');
+
+//question.style.display = "none";
+
+//bookmark
+
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+let bookcheck = localStorage.getItem("bookmark");
+
+const bookmarkOn = () => {
+  bookmark.classList.add("quiz-card__bookmark--active");
+  localStorage.setItem("bookmark", "on");
+};
+
+const bookmarkOff = () => {
+  bookmark.classList.remove("quiz-card__bookmark--active");
+  localStorage.setItem("bookmark", "off");
+};
+
+if (bookcheck === "on") {
+  bookmarkOn();
+}
+
+bookmark.addEventListener("click", () => {
+  bookcheck = localStorage.getItem("bookmark");
+  if (bookcheck === "on") {
+    bookmarkOff();
+  } else {
+    bookmarkOn();
+  }
+});
