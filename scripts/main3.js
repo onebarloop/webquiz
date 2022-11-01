75f5df808280675d7d9a792d422891e2b6346f9a
Alexander Bayer <alexanderbayer@posteo.de>
2022-11-01 11:12:08 +0100

    Add bookmark-functionality for question 2

--- scripts/main3.js
+++ scripts/main3.js
@@ -26,3 +26,30 @@ bookmark.addEventListener("click", () => {
     bookmarkOn();
   }
 });
+
+const bookmark2 = document.querySelector('[data-js="bookmark2"]');
+
+let bookcheck2 = localStorage.getItem("bookmark2");
+
+const bookmark2On = () => {
+  bookmark2.classList.add("quiz-card__bookmark--active");
+  localStorage.setItem("bookmark2", "on");
+};
+
+const bookmark2Off = () => {
+  bookmark2.classList.remove("quiz-card__bookmark--active");
+  localStorage.setItem("bookmark2", "off");
+};
+
+if (bookcheck2 === "on") {
+  bookmark2On();
+}
+
+bookmark2.addEventListener("click", () => {
+  bookcheck2 = localStorage.getItem("bookmark2");
+  if (bookcheck2 === "on") {
+    bookmark2Off();
+  } else {
+    bookmark2On();
+  }
+});
