f5b64e73453f7cd1c688bca4946d73cf347385e3
Alexander Bayer <alexanderbayer@posteo.de>
2022-11-04 15:09:09 +0100

    Finish form- functionality

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,12 @@
 const form = document.querySelector('[data-js="form"]');
 
+const main = document.querySelector('[data-js="main"]');
+
 const counterQuestion = document.querySelector('[data-js="counter-question"]');
 
 const counterAnswer = document.querySelector('[data-js="counter-answer"]');
+
+const submitButton = document.querySelector('[data-js="submit-button"]');
 
 //Counter
 
@@ -21,4 +25,52 @@ form.elements.answer.addEventListener("input", (event) => {
 
 //Cardgenerator
 
-const generateCard = (data) => {};
+const generateCard = (data) => {
+  const box = document.createElement("section");
+  box.classList.add("quiz-card");
+  main.append(box);
+
+  const questionBox = document.createElement("p");
+  questionBox.classList.add("quiz-card__question");
+  questionBox.textContent = data.question;
+  box.append(questionBox);
+
+  const answerBox = document.createElement("p");
+  answerBox.classList.add("quiz-card__answer");
+  answerBox.textContent = data.answer;
+  box.append(answerBox);
+
+  const tagBox = document.createElement("div");
+  tagBox.classList.add("quiz-card__tagbox");
+  box.append(tagBox);
+
+  const tagButton = document.createElement("button");
+  tagButton.classList.add("quiz-card__tag");
+  tagButton.textContent = `#${data.tag}`;
+  tagBox.append(tagButton);
+};
+
+// Form Submit
+
+let cardCount = 0;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userInput = {
+    question: event.target.question.value,
+    answer: event.target.answer.value,
+    tag: event.target.tag.value,
+  };
+  if (cardCount >= 4) {
+    submitButton.textContent = "Stop!";
+    submitButton.setAttribute("style", "background-color : red");
+    return;
+  } else {
+    generateCard(userInput);
+    cardCount++;
+    counterQuestion.textContent = "160 Zeichen übrig";
+    counterAnswer.textContent = "160 Zeichen übrig";
+    event.target.reset();
+    event.target.question.focus();
+  }
+});
